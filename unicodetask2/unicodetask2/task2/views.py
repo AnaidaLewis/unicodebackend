40f1f6a9f0eee3cd4824148d1ac049c0ea49cff6
Mac <mac@Macs-iMac.local>
2021-07-26 14:55:43 +0530

    version2

--- unicodetask2/unicodetask2/task2/views.py
+++ unicodetask2/unicodetask2/task2/views.py
@@ -9,5 +9,5 @@ def home(response):
     return HttpResponse(" <h1>Home Page</h1> ")
 
 def types(request):
-    response = requests.get('https://pokeapi.co/api/v2/type/').json()
+    response = requests.get('https://pokeapi.co/api/v2/pokemon?limit=100').json()
     return render(request, 'task2/home.html',{'response':response})
