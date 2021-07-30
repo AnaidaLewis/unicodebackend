deebe31b313c41e369853bc48582cb5e2f73dc1b
Mac <mac@Macs-iMac.local>
2021-07-30 20:19:46 +0530

    version2

--- unicodetask3/task3/views.py
+++ unicodetask3/task3/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseNotFound
 import requests
-import json
+import json 
 from .forms import PokemonType
 
 # Create your views here.
@@ -15,12 +15,14 @@ def create(request):
         if form.is_valid():
             n = form.cleaned_data["name"]
             a = n.lower()
-            b = "https://pokeapi.co/api/v2/type/" + a
-            t = requests.get(b).json()
-            return render(request,'task3/type_name.html',{'t':t})
+            if (a=="normal") or (a=="fighting") or (a=="flying") or (a=="poison") or (a=="ground") or (a=="rock") or (a=="ghost") or (a=="steel") or (a=="fire") or (a=="water") or (a=="grass") or (a=="elctric") or (a=="psychic") or (a=="ice") or (a=="dragon") or (a=="dark") or (a=="fairy") or (a=="unknown") or (a=="shadow"): 
+                b = "https://pokeapi.co/api/v2/type/" + a
+                t = requests.get(b).json()
+                return render(request,'task3/type_name.html',{'t':t})
+            else:
+                return render(request,'task3/invalid_error.html')
 
-
-    else:
+    else:              
         form = PokemonType()
 
     response = requests.get("https://pokeapi.co/api/v2/type/").json()
